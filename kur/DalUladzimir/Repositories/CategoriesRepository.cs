82ecc1789286c226a9e77d0e31be274bee166b30
Bubeee <voweer@gmail.com>
2014-12-17 00:16:17 +0300

    [Type Id and so on]

--- kur/DalUladzimir/Repositories/CategoriesRepository.cs
+++ kur/DalUladzimir/Repositories/CategoriesRepository.cs
@@ -26,9 +26,9 @@ namespace DalUladzimir.Repositories
         {
           while (reader.Read())
           {
-            category.Id = (int) reader[0];
-            category.Name = (string) reader[1];
-            category.Image = (string) reader[2];
+            category.Id = (int)reader[0];
+            category.Name = (string)reader[1];
+            category.Image = (string)reader[2];
           }
         }
       }
@@ -54,9 +54,9 @@ namespace DalUladzimir.Repositories
           {
             var category = new Category
             {
-              Id = (int) reader[0], 
-              Name = (string) reader[1], 
-              Image = (string) reader[2]
+              Id = (int)reader[0],
+              Name = (string)reader[1],
+              Image = (string)reader[2]
             };
 
             categories.Add(category);
@@ -69,7 +69,24 @@ namespace DalUladzimir.Repositories
 
     public int Create(Category model)
     {
-      throw new System.NotImplementedException();
+      int newId;
+      var conString = ConfigurationManager.ConnectionStrings["kurVova"].ConnectionString;
+      var query = "INSERT INTO [dbo].[Categories] ([Name], [Picture]) " +
+                  "VALUES (@Name, @Image); " +
+                  "SELECT CAST(scope_identity() AS int)";
+
+      using (var connection = new SqlConnection(conString))
+      {
+        using (var command = new SqlCommand(query, connection))
+        {
+          command.Parameters.Add(new SqlParameter("@Name", model.Name));
+          command.Parameters.Add(new SqlParameter("@Image", model.Image));
+          connection.Open();
+          newId = (int)command.ExecuteScalar();
+        }
+      }
+
+      return newId;
     }
   }
 }
--- kur/Interfaces/Entities/ProductCreate.cs
+++ kur/Interfaces/Entities/ProductCreate.cs
@@ -6,27 +6,28 @@ using System.Threading.Tasks;
 
 namespace Interfaces.Entities
 {
-    public class ProductCreate
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int ManufacturerId { get; set; }
-        public int Price { get; set; }
-        public string Warranty { get; set; }
-        public int DeliveryId { get; set; }
-        public string Picture { get; set; }
-        public int? Count { get; set; }
-        public string StorageSerial { get; set; }//???
-        public List<string> Attributes { get; set; }
-        public ProductTypeCreate ProductType { get; set; }
+  public class ProductCreate
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ManufacturerId { get; set; }
+    public int Price { get; set; }
+    public string Warranty { get; set; }
+    public int DeliveryId { get; set; }
+    public string Picture { get; set; }
+    public int? Count { get; set; }
+    public int TypeId { get; set; }
+    public string StorageSerial { get; set; }//???
+    public List<string> Attributes { get; set; }
+    public ProductTypeCreate ProductType { get; set; }
 
-        public ProductCreate()
-        {
-            Attributes = new List<string>();
-        }
-        public override string ToString()
-        {
-            return Id + " " + Name + " " + Price + " " + Warranty + " " + " " + Picture + " " + Count;
-        }
+    public ProductCreate()
+    {
+      Attributes = new List<string>();
     }
+    public override string ToString()
+    {
+      return Id + " " + Name + " " + Price + " " + Warranty + " " + " " + Picture + " " + Count;
+    }
+  }
 }
